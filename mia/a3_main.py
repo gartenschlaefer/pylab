d0b4a4e90ffd302b9bf1c15bb6230b9d068c9e92
chrisworld3 <christian.walter@student.tugraz.at>
2019-10-21 14:16:27 +0200

    a3.1 nearly finished

--- mia/a3_main.py
+++ mia/a3_main.py
@@ -24,11 +24,14 @@ if __name__ == '__main__':
   ol = N // 2
   hop = N - ol
 
-  n_frames = len(x) / hop
+  n_frames = len(x) // hop
   print("n_frames: ", n_frames)
 
   # some vectors
   f = np.arange(0, fs/2, fs/N)
+  frames = np.linspace(0, hop/fs * n_frames, n_frames)
+
+  print("time: ", hop/fs * n_frames)
 
   # windowing
   w = np.hanning(N)
@@ -45,24 +48,32 @@ if __name__ == '__main__':
   # log
   Y = 20 * np.log10(2 / N * np.abs(X[:, 0:512]))
 
-  # some frame to plot
-  fi = int(n_frames // 3) 
+  # peaks, freq and ampl layout with three partial tones
+  peaks = np.empty((0, 4), float)
+  ampl = np.empty((0, 4), float)
+  ampl_est = np.empty((0, 4), float)
+  f_est = np.empty((0, 4), float)
 
-  # peaks layout with three partial tones
-  peaks = np.array([0, 0, 0, 0])
-  #f_est = np.array([0, 0, 0, 0])
+
+  frames_impact = np.zeros(int(n_frames))
 
   # save all peaks
   for fi in np.arange(Y.shape[0]):
 
     # find peaks
-    p, _ = signal.find_peaks(Y[fi], height=(40, 100))
+    p, v = signal.find_peaks(Y[fi], height=(40, 100))
 
     # check if all three partial tones are included 
     if p.size != 0 and p[0:4].shape[0] == 4:
 
-      # save peaks
+      # save peaks and amplitudes directly
       peaks = np.vstack((peaks, p[0:4]))
+      ampl = np.vstack((ampl, v["peak_heights"][0:4]))
+
+      # parabolic interpolation for amplitude
+      ampl_est = np.vstack((ampl_est, parabol_interp(Y[fi], p[0:4])[1]))
+
+      frames_impact[fi] = 1;
 
       # save instantaneous frequency estimation
       #f_est = np.vstack((f_est, inst_f(X, fi, p[0:4], hop, N, fs)))
@@ -74,26 +85,113 @@ if __name__ == '__main__':
   f_est = np.load('f_est.npy')
 
   # remove first entry
-  peaks = np.delete(peaks, 0, 0)
   f_est = np.delete(f_est, 0, 0)
 
-
+  # frequency should be at c1 = 261,626Hz
   print("-- frequency:")
   print("mean peaks f: ", np.mean(peaks, 0) * fs / N)
   print("mean estimated f: ", np.mean(f_est, 0))
+  print("-- Amplitude:")
+  print("mean peak A: ", np.mean(ampl, 0))
+  print("mean estimated A: ", np.mean(ampl_est, 0))
 
-  # frequency should be at c1 = 261,626Hz
 
-  # plot something
-  # plt.figure(12)
+  # --
+  # determine modulation params
+
+  # get start frame idx of significt frmes
+  n_start_frames = np.where(frames_impact == 1)[0][0]
+  n_end_frames = n_frames - n_start_frames - peaks.shape[0]
+
+  # interesting times
+  frame_cue = np.concatenate((np.zeros(2 * fs//hop), np.ones(1 * fs//hop), np.zeros(n_frames - 3 * fs//hop)));
+
+  # some zero padding of the peaks
+  ampl_est = np.pad(ampl_est, [(n_start_frames, n_end_frames), (0, 0)], 'constant', constant_values=0)
+  f_est = np.pad(f_est, [(n_start_frames, n_end_frames), (0, 0)], 'constant', constant_values=0)
+
+  # params of modulation
+  delta_ampl = (np.max(ampl_est[frame_cue==1], 0) - np.min(ampl_est[frame_cue==1], 0)) / 2
+  delta_f = (np.max(f_est[frame_cue==1], 0) - np.min(f_est[frame_cue==1], 0)) / 2
+
+  print("-- Amplitude Modulation:")
+  print("delta_ampl: ", delta_ampl)
+  print("-- Frequency Modulation:")
+  print("delta_f: ", delta_f)
+
+
+
+  #print('frame_cues: ', frame_cue.shape)
+
+  # # --
+  # # plot amplitude modulation only cues
+  # #
+  # plt.figure(2, figsize=(8, 4))
+  # [f1, f2, f3, f4] = plt.plot(frames[frame_cue==1], ampl_est[frame_cue==1])
+  # plt.ylabel('magnitude [dB]')
+  # plt.xlabel('time [s]')
+  # plt.legend([f1, f2, f3, f4], ["ampl. f0","ampl. f1","ampl. f2","ampl. f3"], loc=1)
+  # plt.savefig('amp_mod_est_cue.png', dpi=150)
+
+  # # --
+  # # plot frequency modulation
+  # #
+  # plt.figure(3, figsize=(8, 4))
+  # [f1, f2, f3, f4] = plt.plot(frames[frame_cue==1], f_est[frame_cue==1], label='frequency')
+  # plt.ylabel('frequency [Hz]')
+  # plt.xlabel('time [s]')
+  # plt.legend([f1, f2, f3, f4], ["f0","f1","f2","f3"], loc=1)
+  # plt.savefig('freq_mod_est_cue.png', dpi=150)
+
+  # plt.show()
+
+
+
+  # # --
+  # # plot amplitude modulation
+  # #
+  # plt.figure(2, figsize=(8, 4))
+  # [f1, f2, f3, f4] = plt.plot(frames[frames_impact==1], ampl_est)
+  # plt.ylabel('magnitude [dB]')
+  # plt.xlabel('time [s]')
+  # plt.legend([f1, f2, f3, f4], ["ampl. f0","ampl. f1","ampl. f2","ampl. f3"], loc=1)
+  # plt.savefig('amp_mod_est.png', dpi=150)
+
+  # # --
+  # # plot frequency modulation
+  # #
+  # plt.figure(3, figsize=(8, 4))
+  # [f1, f2, f3, f4] = plt.plot(frames[frames_impact==1], f_est, label='frequency')
+  # plt.ylabel('frequency [Hz]')
+  # plt.xlabel('time [s]')
+  # plt.legend([f1, f2, f3, f4], ["f0","f1","f2","f3"], loc=1)
+  # #plt.savefig('freq_mod_est.png', dpi=150)
+  
+  # plt.show()
+
+
+  # --
+  # plot bariton dft
+  #
+  # some frame to plot
+  # fi = int(n_frames // 2)
+  # print("frame at time: ", hop/fs * fi)
+
+  # p, _ = signal.find_peaks(Y[fi], height=(40, 100))
+
+  # plt.figure(1, figsize=(8, 4))
   # #plt.subplot(211)
   # plt.plot(f, Y[fi], label='bariton')
   # plt.scatter(p[0:4] * fs / N, Y[fi][p[0:4]], label='peaks')
 
+  # plt.title('Baritonvokal frame at time: 5.677s')
+  # plt.ylabel('magnitude [dB]')
+  # plt.xlabel('frequency [Hz]')
   # plt.grid()
-  # plt.xlabel('frequency')
-  # plt.ylabel('magnitude')
+
+  # plt.xlim(0, 5000)
   # plt.legend()
+  # plt.savefig('bar_dft.png', dpi=150)
   # plt.show()
 
 
@@ -105,7 +203,7 @@ if __name__ == '__main__':
   # plt.ylim(0, 3000)
   # plt.show()
 
-  plt.figure(1)
-  plt.specgram(x, 1024, fs)
-  plt.ylim(0, 2000)
-  plt.show()
+  # plt.figure(1)
+  # plt.specgram(x, 1024, fs)
+  # plt.ylim(0, 2000)
+  # plt.show()
